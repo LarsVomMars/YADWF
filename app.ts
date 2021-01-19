028220a31c9deb55b4d184415250b8f7807cfdbb
LarsVomMars <developer@kroenner.eu>
2021-01-19 15:25:30 +0100

    METHODS

--- app.ts
+++ app.ts
@@ -17,7 +17,6 @@ export class Application {
     for await (const req of this.#server) {
       const c = new Context(req);
       console.log(c.method, c.path, c.query, c.url.toString());
-
       const hdl = this.#router.find(HTTPMethods.indexOf(c.method), c.path);
       await hdl(c);
       req.respond(c.response);
@@ -28,8 +27,48 @@ export class Application {
     this.startServer(serve(options));
   }
 
-  get(path: string, handler: Handler) {
+  get(path: string, handler: Handler): Application {
     this.#router.add(Method.GET, path, handler);
+    return this;
+  }
+
+  post(path: string, handler: Handler): Application {
+    this.#router.add(Method.POST, path, handler);
+    return this;
+  }
+
+  put(path: string, handler: Handler): Application {
+    this.#router.add(Method.PUT, path, handler);
+    return this;
+  }
+
+  delete(path: string, handler: Handler): Application {
+    this.#router.add(Method.DELETE, path, handler);
+    return this;
+  }
+
+  trace(path: string, handler: Handler): Application {
+    this.#router.add(Method.TRACE, path, handler);
+    return this;
+  }
+
+  options(path: string, handler: Handler): Application {
+    this.#router.add(Method.OPTIONS, path, handler);
+    return this;
+  }
+
+  patch(path: string, handler: Handler): Application {
+    this.#router.add(Method.PATCH, path, handler);
+    return this;
+  }
+
+  connect(path: string, handler: Handler): Application {
+    this.#router.add(Method.CONNECT, path, handler);
+    return this;
+  }
+
+  head(path: string, handler: Handler): Application {
+    this.#router.add(Method.HEAD, path, handler);
     return this;
   }
 }
