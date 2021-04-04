a11692dcd2c7f3c5ad1964a9fed8b6f33f2afc21
LarsVomMars <developer@kroenner.eu>
2021-04-04 22:53:04 +0200

    Bump to std@0.92

--- deps.ts
+++ deps.ts
@@ -6,18 +6,18 @@ export {
   ServerRequest,
   setCookie,
   Status,
-} from "https://deno.land/std@0.90.0/http/mod.ts";
+} from "https://deno.land/std@0.92.0/http/mod.ts";
 
 export type {
   Cookie,
   HTTPOptions,
   Response,
-} from "https://deno.land/std@0.90.0/http/mod.ts";
+} from "https://deno.land/std@0.92.0/http/mod.ts";
 
-export { MultipartReader } from "https://deno.land/std@0.90.0/mime/mod.ts";
+export { MultipartReader } from "https://deno.land/std@0.92.0/mime/mod.ts";
 
-export * as log from "https://deno.land/std@0.90.0/log/mod.ts";
+export * as log from "https://deno.land/std@0.92.0/log/mod.ts";
 
-export { format } from "https://deno.land/std@0.90.0/datetime/mod.ts";
+export { format } from "https://deno.land/std@0.92.0/datetime/mod.ts";
 
-export { extname, join, sep } from "https://deno.land/std@0.90.0/path/mod.ts";
+export { extname, join, sep } from "https://deno.land/std@0.92.0/path/mod.ts";
